495a33586b21b0632c6c0b29789a92d70366c497
immortalll1 <immortal3309@gmail.com>
2026-06-13 12:29:45 +0300

    Update model.py

--- src/model.py
+++ src/model.py
@@ -3,34 +3,49 @@ import torch.nn as nn
 
 
 class Sign3DCNN(nn.Module):
-    def __init__(self, num_classes: int):
-        super(Sign3DCNN, self).__init__()
+
+    def __init__(self, num_classes):
+        super().__init__()
 
         self.features = nn.Sequential(
-            nn.Conv3d(3, 8, kernel_size=3, padding=1),
+
+            nn.Conv3d(3, 32, kernel_size=3, padding=1),
+            nn.BatchNorm3d(32),
             nn.ReLU(inplace=True),
             nn.MaxPool3d((1, 2, 2)),
 
-            nn.Conv3d(8, 16, kernel_size=3, padding=1),
+            nn.Conv3d(32, 64, kernel_size=3, padding=1),
+            nn.BatchNorm3d(64),
             nn.ReLU(inplace=True),
             nn.MaxPool3d((2, 2, 2)),
 
-            nn.Conv3d(16, 32, kernel_size=3, padding=1),
+            nn.Conv3d(64, 128, kernel_size=3, padding=1),
+            nn.BatchNorm3d(128),
             nn.ReLU(inplace=True),
             nn.MaxPool3d((2, 2, 2)),
+
+            nn.Conv3d(128, 256, kernel_size=3, padding=1),
+            nn.BatchNorm3d(256),
+            nn.ReLU(inplace=True),
 
             nn.AdaptiveAvgPool3d((1, 1, 1))
         )
 
         self.classifier = nn.Sequential(
             nn.Flatten(),
-            nn.Linear(32, 64),
+
+            nn.Linear(256, 512),
             nn.ReLU(inplace=True),
-            nn.Dropout(0.3),
-            nn.Linear(64, num_classes)
+            nn.Dropout(0.5),
+
+            nn.Linear(512, 256),
+            nn.ReLU(inplace=True),
+            nn.Dropout(0.4),
+
+            nn.Linear(256, num_classes)
         )
 
-    def forward(self, x: torch.Tensor) -> torch.Tensor:
+    def forward(self, x):
         x = self.features(x)
         x = self.classifier(x)
         return x
